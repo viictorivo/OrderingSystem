1751ea71a03ff9c4ad8f8c505028991473d4fc1c
Victor Ivo Henrique <38409046+viictorivo@users.noreply.github.com>
2024-09-05 23:31:20 -0300

    ajust variables

--- variables.tf
+++ variables.tf
@@ -1,29 +1,3 @@
-#######modules/eks/variables.tf
-
-variable "aws_public_subnet" {}
-
-variable "vpc_id" {}
-
-variable "cluster_name" {}
-
-variable "endpoint_private_access" {}
-
-variable "endpoint_public_access" {}
-
-variable "public_access_cidrs" {}
-
-variable "node_group_name" {}
-
-variable "scaling_desired_size" {}
-
-variable "scaling_max_size" {}
-
-variable "scaling_min_size" {}
-
-variable "instance_types" {}
-
-variable "key_pair" {}
-
-variable "aws_access_key_id" {}
-
-variable "aws_secret_access_key" {}
+variable "aws_session_token" {
+  type = string
+}
