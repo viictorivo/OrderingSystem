9795d823411dbdfddca8eb2125642fce43904e5d
Victor Ivo Henrique <38409046+viictorivo@users.noreply.github.com>
2024-09-07 22:36:08 -0300

    other try

--- main.tf
+++ main.tf
@@ -1,19 +1,29 @@
-provider "aws" {
-  region = var.aws_region
-}
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.25"
-  subnets         = var.subnets
-  vpc_id          = var.vpc_id
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t2.micro"
-    }
-  }
+  source                  = "./modules/eks"
+  aws_public_subnet       = module.vpc.aws_public_subnet
+  vpc_id                  = module.vpc.vpc_id
+  cluster_name            = "module-eks-${random_string.suffix.result}"
+  endpoint_public_access  = true
+  endpoint_private_access = false
+  public_access_cidrs     = ["0.0.0.0/0"]
+  node_group_name         = "cloudquicklabs"
+  scaling_desired_size    = 1
+  scaling_max_size        = 1
+  scaling_min_size        = 1
+  instance_types          = ["t3.large"]
+  key_pair                = "TestKeyPair"
+}
+
+module "vpc" {
+  source                  = "./modules/vpc"
+  tags                    = "cloudquicklabs"
+  instance_tenancy        = "default"
+  vpc_cidr                = "10.0.0.0/16"
+  access_ip               = "0.0.0.0/0"
+  public_sn_count         = 2
+  public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]
+  map_public_ip_on_launch = true
+  rt_route_cidr_block     = "0.0.0.0/0"
+
 }
