7b1bb6278be99313a037f71f54116c4aa077d47c
Victor Ivo Henrique <38409046+viictorivo@users.noreply.github.com>
2024-09-16 20:12:57 -0300

    try

--- src/Infrastructure/Apis/prisma.service.ts
+++ src/Infrastructure/Apis/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../../prisma/generated';
+import { PrismaClient } from '../../../prisma/generated/index';
 
 @Injectable()
 export class PrismaService
